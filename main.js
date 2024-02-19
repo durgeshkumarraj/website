9d0b494a8a5dad860d7a7954b29d90d652c3e8df
durgesh kumar <131584021+durgeshkumarraj@users.noreply.github.com>
2024-02-20 00:33:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".text", {
+    strings :["Coding Enthusiast", "Open Source contributor", "Frontend Developer", "Graphic Desiner"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    lopp: true
+});
